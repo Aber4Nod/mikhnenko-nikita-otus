e65ae52f0a3998cbf4a802ce69c1def73e4ac46e
Никита Михненко <n.mikhnenko@MacBook-Pro-Nikita.local>
2019-08-01 19:25:21 +0300

    javascript-2: fixing comments - removing catch from promiseReduce

--- javascript/javascript-2/javascript-2.js
+++ javascript/javascript-2/javascript-2.js
@@ -1,12 +1,10 @@
 "use strict";
-const assert = require('assert');
+const chai = require('chai');
 
 function promiseReduce(asyncFunctions, reduce, initialValue) {
     return asyncFunctions.reduce((promise, func) => {
         return promise.then(async currentValue => {
-            let result = await func().catch(error => {
-                console.log(error);
-            });
+            let result = await func();
             return result !== undefined ?
                 reduce(currentValue, result) :
                 currentValue;
@@ -28,13 +26,13 @@ describe('promiseReduce', () => {
             console.log("f3");
             setTimeout(() => resolve(2), 1000)
         });
-        assert.equal(await promiseReduce(
+        chai.expect(await promiseReduce(
             [fn1, fn2, fn3],
             function(memo, value) {
                 return memo * value
             },
             1,
-        ), 8);
+        )).to.equal(8);
     });
     it('fail and success asyncFunctions promises', async () => {
         let fn1 = () => {
@@ -46,12 +44,12 @@ describe('promiseReduce', () => {
         let fn3 = () => new Promise(resolve => {
             setTimeout(() => resolve(2), 1000)
         });
-        assert.equal(await promiseReduce(
+        await promiseReduce(
             [fn1, fn2, fn3],
             function(memo, value) {
                 return memo * value
             },
             1,
-        ), 4);
+        ).catch(error => chai.expect(error.message).to.equal("unexpected error"));
     });
 });
